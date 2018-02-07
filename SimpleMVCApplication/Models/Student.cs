d74c86c2f0a9d8907ef67a810a8793c7577ce4ad
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-07 21:16:36 +0100

    Create new model Student.

---
+++ SimpleMVCApplication/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVCApplication.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Year { get; set; }
+    }
+}
