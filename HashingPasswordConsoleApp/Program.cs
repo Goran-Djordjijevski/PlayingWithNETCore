5e85d6d11258aca0206dcca07d565c06f730377c
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-07 20:41:26 +0100

    Added code for hashing.

--- HashingPasswordConsoleApp/Program.cs
+++ HashingPasswordConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
 
 namespace HashingPasswordConsoleApp
 {
@@ -6,7 +8,26 @@ namespace HashingPasswordConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
 
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string convertSalt = Convert.ToBase64String(salt);
+            Console.WriteLine($"Salt: {convertSalt}");
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8));
+
+            Console.WriteLine($"Hashed: {hashed}");
 
             Console.ReadLine();
         }
