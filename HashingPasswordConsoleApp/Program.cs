2d96d959fe50b6ea74d88ad2cb3cb1ae0ace86ed
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-07 20:43:37 +0100

    Refactor code.

--- HashingPasswordConsoleApp/Program.cs
+++ HashingPasswordConsoleApp/Program.cs
@@ -8,9 +8,29 @@ namespace HashingPasswordConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a password: ");
-            string password = Console.ReadLine();
+            string password = Password();
 
+            byte[] salt = Salt();
+
+            CreateHashPassword(password, salt);
+
+            Console.ReadLine();
+        }
+
+        private static void CreateHashPassword(string password, byte[] salt)
+        {
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                            password: password,
+                            salt: salt,
+                            prf: KeyDerivationPrf.HMACSHA1,
+                            iterationCount: 1000,
+                            numBytesRequested: 256 / 8));
+
+            Console.WriteLine($"Hashed: {hashed}");
+        }
+
+        private static byte[] Salt()
+        {
             byte[] salt = new byte[128 / 8];
             using (var rng = RandomNumberGenerator.Create())
             {
@@ -19,17 +39,14 @@ namespace HashingPasswordConsoleApp
 
             string convertSalt = Convert.ToBase64String(salt);
             Console.WriteLine($"Salt: {convertSalt}");
+            return salt;
+        }
 
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 1000,
-                numBytesRequested: 256 / 8));
-
-            Console.WriteLine($"Hashed: {hashed}");
-
-            Console.ReadLine();
+        private static string Password()
+        {
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+            return password;
         }
     }
 }
